f60f49de9fdd591970c8b51522addc84b1c97bef
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2024-06-09 14:17:17 +0200

    Fix

---
+++ mysite/settings/prod.py
@@ -0,0 +1,17 @@
+# import os
+# from dotenv import load_dotenv
+#
+# DEBUG=False
+# ALLOWED_HOSTS = ['*']
+# load_dotenv()
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'HOST': os.getenv('DB_HOST'),
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'PORT': os.getenv('DB_PORT'),
+#     }
+# }
